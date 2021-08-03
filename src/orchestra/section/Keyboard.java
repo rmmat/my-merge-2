b7b6ddb77217c81977dfff471c694b775b207a41
Richard Matsumoto <richard.matsumoto@gmail.com>
2021-08-03 00:45:48 -0400

    update Percussion and Keyboard

--- src/orchestra/section/Keyboard.java
+++ src/orchestra/section/Keyboard.java
@@ -4,5 +4,6 @@ public class Keyboard extends Section {
     public Keyboard() {
         super("Keyboard");
         addInsrument("Piano");
+        addInsrument("Organ");
     }
 }
--- src/orchestra/section/Percussion.java
+++ src/orchestra/section/Percussion.java
@@ -4,5 +4,7 @@ public class Percussion extends Section {
     public Percussion() {
         super("Percussion");
         addInsrument("Snare Drum");
+        addInsrument("Bass Drum");
+        addInsrument("Timpani");
     }
 }
